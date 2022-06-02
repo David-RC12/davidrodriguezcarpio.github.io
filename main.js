ca8a10895d6fe70748a0f6b7aea0f49bfbe534b3
David-RC12 <80421288+David-RC12@users.noreply.github.com>
2022-06-02 11:32:23 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
